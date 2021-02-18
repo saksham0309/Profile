6b5afdde485e2597eb980d18b4d5f03e9cc56b26
Saksham Mishra <55156585+saksham0309@users.noreply.github.com>
2021-02-18 18:10:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Designer", "Developer", "Guitarist", "Illustrator", "Freelancer"],
+        strings: ["Designer", "Developer", "Illustrator", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
